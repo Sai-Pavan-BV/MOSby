dc0fabebe52dfb6899f122691a64f4dc54e16c19
pavan-deretard <bethina_b220771ee@nitc.ac.in>
2024-06-10 22:28:42 +0530

    mux

--- alu/alu.v
+++ alu/alu.v
@@ -1,5 +1,12 @@
-module alu(clk,rst,op,operand_1,operand_2,status,result,status_out);
-input wire clk,rst;
+module alu(clk,rst,en,op,operand_1,operand_2,status,result_buff,status_out);
+input wire clk,rst,en;
 input wire[7:0] op,operand_1,operand_2,status;
-output reg[7:0] result,status_out
+output reg[7:0] result_buff,status_out;
+reg result
+parameter ADD,ADC,CMP,AND,EOR,ORA,BIT,ASL,LSR,ROL,ROR;
+
+case (op)
+    ADD: 
+    default: 
+endcase
 endmodule
---
+++ mux/mux_8x1.v
@@ -0,0 +1,16 @@
+module mux_8x1 #(width) (i0,i1,i2,i3,i4,i5,i6,i7,select,en,out);
+input wire[width-1:0] i0,i1,i2,i3,i4,i5,i6,i7;
+input wire[2:0] select;
+output wire[width-1:0] out;
+assign out=en?
+            case (select)
+                0:i0
+                1:i1
+                2:i2
+                3:i3
+                4:i4
+                5:i5
+                6:i6
+                7:i7  
+            endcase:(width)'dz;
+endmodule
