6a40e5671b56293dd35d39ad6e14c487c4a7ffea
pavan-deretard <bethina_b220771ee@nitc.ac.in>
2024-06-11 19:31:21 +0530

    nil

--- alu/l_shift.v
+++ alu/l_shift.v
@@ -1,15 +1,22 @@
 module l_shift(
-    en,sh_r,operand,
-    ,cout,result
+    clk,en,sh_r,operand,
+    cout,result
 );
-input wire en,sh_r;
-input wire[7-:8] operand;
-output wire[7-:8] result;
+input wire clk,en,sh_r;
+input wire[7:0] operand;
+output wire[7:0] result;
+output wire cout;
+reg cout_buff;
 
-reg[7-:8] result_buff;
+reg[7:0] result_buff;
 
-assign {cout_buff,result}={operand,1b'0}>>1
+always @(posedge clk ) begin
+    cout_buff=operand[7];
+    result_buff={operand[6:0],1'b0};
+end
 
+assign result=en?result_buff:8'hzz;
+assign cout=en?cout_buff:1'bz;
 
 
 endmodule
