7e11ef16d144b3ad5a87ab8ffea6cac6b4e8df4f
pavan-deretard <bethina_b220771ee@nitc.ac.in>
2024-06-12 21:42:31 +0530

    nil

---
+++ alu/r_shift.v
@@ -0,0 +1,19 @@
+module r_shift(
+    en,sh_r,cin,operand,
+    cout,result
+);
+input wire en,sh_r,cin;
+input wire[7:0] operand;
+output wire[7:0] result;
+output wire cout;
+wire cout_buff;
+
+wire[7:0] result_buff;
+
+assign {result_buff,cout}=sh_r?{1'b0,operand}:{cin,operand};
+
+assign result=en?result_buff:8'hzz;
+assign cout=en?cout_buff:1'bz;
+
+
+endmodule
