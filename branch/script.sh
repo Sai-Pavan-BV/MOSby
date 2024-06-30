53130d6f0a00ab452928139abada6464234a5ae4
pavan-deretard <bethina_b220771ee@nitc.ac.in>
2024-06-30 18:24:42 +0530

    nil

--- branch/script.sh
+++ branch/script.sh
@@ -3,10 +3,5 @@ clear
 iverilog -o temp branch.v
 rm temp
 git add --all
-nill=""
-if [ $1 == $nill ]
-then
-    git commit -m"nil"
-else
-    git commit -m "$1"
-fi
+git commit --reuse-message=HEAD
+
