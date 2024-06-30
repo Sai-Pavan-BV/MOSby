7a69e336572c8ee2565ca4f5e8c68d65b4b2c4b1
pavan-deretard <bethina_b220771ee@nitc.ac.in>
2024-06-30 18:01:08 +0530

    nil

--- branch/branch.v
+++ branch/branch.v
@@ -16,25 +16,42 @@ parameter bcc = 0, // Branch if carry flag clear
     bpl = 5, // Branch if negative flag clear
     bvc = 6, // Branch if overflow flag clear
     bvs = 7; // Branch if overflow flag set
+
 assign lower_byte=lower_byte_buffer;
 assign normal=reset_counter==0;
 assign branch=branch_uncon|branch_con&test|branch_internal;
-always @(posedge rst ) begin
+
+always @(posedge rst ) begin        //reset sequence
     reset_counter=2;
     lower_byte_buffer=1;
     branch_internal=0;
 end
 
-always @(negedge clk_2 ) begin
-    if(reset_counter ==2) begin
+always @(negedge clk_2 ) begin      
+    if(reset_counter ==2) begin     //branching to reset vector
         lower_byte_buffer=1;
         reset_counter=1;
         branch_internal=0;
     end
-    if (reset_counter==1) begin
+    else if (reset_counter==1) begin
         lower_byte_buffer=0;
         reset_counter=0;
         branch_internal=1;
     end
+    else if(reset_counter==0) begin
+        lower_byte_buffer=0;
+        branch_internal=0;
+    end
+    case(branch_op)             //status reg contents : carry zero interrput_disable decimal_mode break_cmd overflow negative
+        bcc: test = ~status[7];
+        bcs: test = status[7];
+        beq: test = status[6];
+        bmi: test = status[0];
+        bne: test = ~status[6];
+        bpl: test = ~status[0];
+        bvc: test = status[2];
+        bvs: test = status[2];
+    endcase
 end
+
 endmodule
