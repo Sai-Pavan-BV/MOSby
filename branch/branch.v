b78fbd5e5fdadcbec5b93b5389bab595a55ed4cf
pavan-deretard <bethina_b220771ee@nitc.ac.in>
2024-06-30 16:47:21 +0530

    branch adding

--- branch/branch.v
+++ branch/branch.v
@@ -1,10 +1,20 @@
 module branch ();
 wire rst,clk_1,clk_2,branch_uncon,branch_con;
 input wire[7:0] status,data_bus;
-output wire branch;
+output wire branch,lower_byte;
+reg lower_byte_buffer;
 reg[1:0] reset_counter;
+
+assign lower_byte=lower_byte_buffer;
+
 always @(posedge rst ) begin
     reset_counter=2;
+    lower_byte_buffer=0;
 end
-
+always @(negedge clk_2 ) begin
+    if(reset_counter!=0) begin
+        lower_byte_buffer=1;
+        reset_counter=reset_counter-1;
+    end
+end
 endmodule
