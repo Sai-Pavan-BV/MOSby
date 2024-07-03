9de92d7c08aeb20cf05c856b607d23476914c106
pavan-deretard <bethina_b220771ee@nitc.ac.in>
2024-07-03 21:00:21 +0530

    alu updated

--- alu/l_shift.v
+++ alu/l_shift.v
@@ -10,7 +10,7 @@ wire cout_buff;
 
 wire[7:0] result_buff;
 
-assign {cout,result_buff}=sh_r?{operand,1'b0}:{operand,cin};
+assign {cout_buff,result_buff}=sh_r?{operand,1'b0}:{operand,cin};
 
 assign result=en?result_buff:8'hzz;
 assign cout=en?cout_buff:1'bz;
